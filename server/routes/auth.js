a1dec3801f6d9968b214e55f009591b24f6e4bb2
Mohd Aijaz Vali <z4ckdazad@gmail.com>
2025-04-18 20:34:49 +0530

    auth,package

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,10 @@
-import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 import bcrypt from "bcryptjs";
 import User from "../models/User.js";
+import express from "express";
+
 
 const router = express.Router();
 
@@ -38,4 +42,40 @@ router.post("/register", async (req, res) => {
   }
 });
 
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "2h" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+        email: user.email
+      }
+    });
+
+  } catch (err) {
+    console.error("Login error:", err.message);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 export default router;
