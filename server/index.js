e44d4037b0713169fe047105cd5e2af7b736502d
Mohd Aijaz Vali <z4ckdazad@gmail.com>
2025-04-19 14:56:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://alumni-connect-portal.vercel.app"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 connectDB();
