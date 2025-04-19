af065a19a11e10339a0bdc8c6bf08190cf8d01e8
MohdKashif114 <151407324+aijazvali@users.noreply.github.com>
2025-04-19 22:56:40 +0530

    added kashifs code

--- server/controllers/alumnifetch.js
+++ server/controllers/alumnifetch.js
@@ -2,13 +2,15 @@ import User from "../models/User.js";
 
 export default async function getFilteredAlumni(req, res) {
   try {
-    const { college, batch, branch } = req.query;
+    const { college, batch, branch,jobtitle,location } = req.query;
     console.log("fetching.....alumniiii")
     const query = { role: "alumni" };
 
     if (college) query.college = { $regex: college, $options: "i" };
     if (batch) query.batch = { $regex: batch, $options: "i" };
     if (branch) query.branch = { $regex: branch, $options: "i" };
+    if (jobtitle) query.jobtitle = { $regex: jobtitle, $options: "i" };
+    if (location) query.location = { $regex: location, $options: "i" };
 
     const alumni = await User.find(query).select("-password");
     res.status(200).json(alumni);
