a0036f74d15ff1e33c6c1600d484f55593229d17
MohdKashif114 <151407324+aijazvali@users.noreply.github.com>
2025-04-19 23:04:25 +0530

    added server link

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -22,7 +22,7 @@ export default function Register() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/register", {
+      const res = await fetch("https://alumni-connect-portal.onrender.com/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
