6f19ba706387dfe8d4e0133561fd6bf0a6042c3c
Mohd Aijaz Vali <z4ckdazad@gmail.com>
2025-04-19 14:48:48 +0530

    fix
    server error due to local host
    instead of "https://alumni-connect-portal.onrender.com"

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -21,7 +21,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://alumni-connect-portal.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
